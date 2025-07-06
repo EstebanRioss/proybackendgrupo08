78f03a5c9126f65298d8e8fc164b261378f9685e
Esteban Rios <esteban_690@hotmail.com>
2025-07-06 02:05:23 -0300

    Se conecta el backend a base de datos

--- database.js
+++ database.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/proyectofinal';
+const URI = process.env.MONGODB_URI;
 
-mongoose.connect(URI)
-  .then(db => console.log('DB is connected'))
-  .catch(err => console.error(err));
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('✅ Conectado a MongoDB Atlas'))
+  .catch(err => console.error('❌ Error conectando a MongoDB:', err));
 
 module.exports = mongoose;
